078058ca81c35bd7ef8f8baabc22ff706368ba27
RobertCalbul <rcalbul2011@alu.uct.cl>
2014-06-04 02:42:30 -0400

    e.e

---
+++ hilos/hilos/ClassLabel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace hilos
+{
+    class ClassLabel
+    {
+        public int id {get;set;}
+        public String nombre { get; set; }
+        public int posx { get; set; }
+        public int posy { get; set; }
+        public Label ele { get; set; }
+        public ClassLabel()
+        { }
+        public ClassLabel( String nombre,int posx, int posy)
+        {
+            this.nombre = nombre;
+            this.posx = posx;
+            this.posy = posy;
+        }
+        public ClassLabel(int id,String nombre, int posx, int posy, Label ele)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.posx = posx;
+            this.posy = posy;
+            this.ele = ele;
+        }
+
+        public Label getLabel()
+        {
+            Label nueva = new Label();
+            //nueva.Margin = new System.Windows.Thickness(this.posx, this.posy,0,0);
+            nueva.Content = this.nombre;
+            nueva.Width = 40;
+            nueva.Height = 40;
+            TranslateTransform translateTransform1 = new TranslateTransform(this.posx, this.posy);
+            nueva.RenderTransform = translateTransform1;
+
+            return nueva;
+        }
+       /* public ClassLabel getLabel()
+        {
+            Label nueva = new Label();
+            //nueva.Margin = new System.Windows.Thickness(this.posx, this.posy,0,0);
+            nueva.Content = this.nombre;
+            nueva.Width = 40;
+            nueva.Height = 40;
+            TranslateTransform translateTransform1 = new TranslateTransform(this.posx, this.posy);
+            nueva.RenderTransform = translateTransform1;
+            //ClassLabel a =
+            return new ClassLabel(this.id, this.nombre, this.posx, this.posy, this.ele);
+        }*/
+
+    }
+}
---
+++ hilos/hilos/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace hilos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Label> labels;
+        Point PositionMouseScreen;
+        public Label LabelSelected = new Label();
+        Point posActual;
+        Point posNext;
+        public MainWindow()
+        {
+            InitializeComponent();
+            labels = new List<Label>(); 
+
+        }
+
+        private void Centro_Drop(object sender, DragEventArgs e)
+        {
+            SolidColorBrush scb = (SolidColorBrush)e.Data.GetData(typeof(SolidColorBrush));
+            
+            Console.WriteLine("scb: "+scb);
+            if (scb == (SolidColorBrush)referencia.Fill) Centro.Fill = scb;
+            else MessageBox.Show("No es el color correcto");
+        }
+
+        private void rec_mouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            Rectangle r = (Rectangle)sender;
+            DataObject Do = new DataObject(r.Fill);
+            DragDrop.DoDragDrop(r,Do,DragDropEffects.All);
+         }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            PositionMouseScreen = e.GetPosition(this.workSpace);
+            pos.Content = "x: " + PositionMouseScreen.X + "   Windoww\ny: " + PositionMouseScreen.Y;
+        }
+
+        int contador = 0;
+        private void lIf_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+            Label a = new ClassLabel("label "+contador,90,10).getLabel();
+            a.MouseDown += new System.Windows.Input.MouseButtonEventHandler(this.label_mouseDown);
+            a.MouseUp += new System.Windows.Input.MouseButtonEventHandler(this.label_mouseUp);
+
+            this.workSpace.Children.Add(a);
+            contador++;
+        }
+
+        private void label_mouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Label l = (Label)sender;
+            LabelSelected = l;
+            posActual = PositionMouseScreen;
+           // Console.WriteLine("DOWN >>>"+posActual.X+" "+posActual.Y);
+            
+        }
+        private void label_mouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+            Console.WriteLine("UP>>>" );
+
+        }
+
+
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            posNext = PositionMouseScreen;
+           
+            double posx = 0;
+            double posy = 0;
+            double tx = this.LabelSelected.Width / 2; 
+            double ty = this.LabelSelected.Height / 2;
+            posx = ((posActual.X-tx)-(posNext.X-tx));
+            posy = ((posActual.Y-ty)-(posNext.Y-ty));
+            Console.WriteLine("<<------------------->>");
+            Console.WriteLine("comparacion actual Next\nx"+posActual.X + ">" + posNext.X + "\ny" + posActual.Y + ">" + posNext.Y);
+            Console.WriteLine("\nposicion final\nx" +( posActual.X - posx )+ " y" + (posActual.Y-posy));
+            TranslateTransform translate = new TranslateTransform(posx*-1, posy*-1);
+            Console.WriteLine("\nposicion invertida\nx" + posx * -1 + " y" + posy * -1);
+            LabelSelected.RenderTransform = translate;
+        }
+
+
+    }
+}
